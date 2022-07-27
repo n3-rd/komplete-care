5a317b40b665b9d9b596e78cd0ab919ff5a7d8d4
n3-rd <godwinjdayo@gmail.com>
2022-07-27 05:05:59 +0100

    Added Dashboard

---
+++ src/components/DashboardContent.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <div>
+        <div class="profile-options flex flex-row flex-end text-primary">
+            <div class="profile-options__item opacity-7">Take a tour</div>
+            <div class="profile-options__item">Item 2</div>
+            <div class="profile-options__item opacity-7">Item 3</div>
+            <div class="profile-options__item">Item 4</div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.profile-options {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-end;
+    margin-top: 20px;
+    margin-right: 20px;
+
+    .profile-options__item {
+        margin-left: 10px;
+        cursor: pointer;
+        font-weight: 600;
+    }
+}
+</style>
---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,49 @@
+<script setup>
+import DashboardSidebarVue from '../components/DashboardSidebar.vue';
+import DashboardContent from '../components/DashboardContent.vue';
+</script>
+
+<template>
+    <div>
+
+        <div class="dashboard">
+
+            <div class="sidebar">
+                <DashboardSidebarVue></DashboardSidebarVue>
+            </div>
+            <div class="content">
+                <DashboardContent></DashboardContent>
+
+            </div>
+
+        </div>
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.dashboard {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+
+    .sidebar {
+        width: 350px;
+        height: 100%;
+        border-right: 1px solid #e5e5e5;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .content {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+}
+</style>
